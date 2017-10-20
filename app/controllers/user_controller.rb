39136585c125cb71c68819a4c627da566d965bc2
Amberly Armstrong <armstrong.amberly@gmail.com>
2017-10-20 20:38:29 +0000

    automatically backed up by learn

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,22 @@
 class UserController < ApplicationController
+
+  get '/login' do
+    if logged_in?
+      redirect "/tweets/index"
+    end
+    erb :"/users/login"
+  end
+
+  post "/login" do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/tweets/index"
+    else
+      redirect "/users/login"
+    end
+  end
+  
   get '/logout' do
     if logged_in?
   		session.clear
