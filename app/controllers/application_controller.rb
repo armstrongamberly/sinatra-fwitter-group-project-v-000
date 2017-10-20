ef86643e9935065fbbb4d6bf7662ad617614b343
Amberly Armstrong <armstrong.amberly@gmail.com>
2017-10-20 20:39:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,57 +13,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    if logged_in?
-      redirect "/tweets/index"
-    end
-    erb :"/users/signup"
-  end
-
-  post "/signup" do
-    if params.has_value?("")
-      redirect "/signup"
-    end
-    @user = User.create(:username => params[:username], email: params[:email], :password => params[:password])
-    session[:user_id] = @user.id
-    redirect '/tweets/index'
-  end
-
-  get '/login' do
-    if logged_in?
-      redirect "/tweets/index"
-    end
-    erb :"/users/login"
-  end
-
-  post "/login" do
-    @user = User.find_by(:username => params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect "/tweets/index"
-    else
-      redirect "/users/login"
-    end
-  end
-
-  get '/tweets/index' do
-    if logged_in?
-      @user = User.find(session[:user_id])
-      erb :"/tweets/index"
-    else
-      redirect "/login"
-    end
-  end
-
-  get '/logout' do
-    if logged_in?
-  		session.clear
-  		redirect "/login"
-    else
-      redirect "/"
-    end
-  end
-
   helpers do
     def logged_in?
       !!session[:user_id]
